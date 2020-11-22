f01b64abe118fb6f54e36c8ae31ea6a4648708aa
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2020-11-23 00:31:32 +0200

    Log temp v2. Correct values including negative

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,40 @@
 #include <Arduino.h>
 
-int sensorPin = 34;
-int sensorValue = 0;
-float vcc = 4.64;
-float dividerK = 1.438 / 2.328;
-float adcV = vcc * dividerK;
+
+int sensorPinPlus = 13;
+int sensorPinMinus = 12;
+float sensorValuePlus = 0;
+float sensorValueMinus = 0;
+float sensorValue = 0;
+float adcV = 3.3;
 int adcResolutionMax = 4095;
-float voltageStep = 0.01 * dividerK; // 10mV / degree - LM35
+float voltageStep = 0.01;
 
 float voltage = 0;
 float temperature = 0;
 
+int filterLen = 1000;
+
+float analogReadAvg(int pin) {
+    float sum = 0;
+    for (int i = 0; i < filterLen; i++) {
+        sum += analogRead(pin);
+    }
+    return sum / filterLen;
+}
+
 void setup() {
+    pinMode(sensorPinPlus, INPUT);
+    pinMode(sensorPinMinus, INPUT);
     Serial.begin(9600);
-    pinMode(sensorPin, INPUT);
 }
 
 void loop() {
-    sensorValue = analogRead(sensorPin);
-    Serial.print("Value: ");
-    Serial.print(sensorValue);
-    Serial.print("; Voltage: ");
+    sensorValuePlus = analogReadAvg(sensorPinPlus);
+    sensorValueMinus = analogReadAvg(sensorPinMinus);
+    sensorValue = sensorValuePlus - sensorValueMinus;
     voltage = sensorValue * adcV / adcResolutionMax;
-    Serial.print(voltage);
-    Serial.print("; Temperature: ");
     temperature = voltage / voltageStep;
-    Serial.print(temperature);
-    Serial.println("");
+    Serial.println(temperature);
     delay(1000);
 }
